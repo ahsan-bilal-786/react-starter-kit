4f9ea00a245959c6008bb62952547afb1b3c193f
Ahsan Bilal <engr.ahsan.bilal@gmail.com>
2020-05-16 15:20:25 +0500

    updating style for header links

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -2,6 +2,14 @@ import styled from 'styled-components';
 import { NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
+export const AppTitle = styled(Link)`
+  color: #fff;
+  &:hover {
+    color: #fff;
+    text-decoration: none;
+  }
+`;
+
 export const UserDropDown = styled(NavDropdown)`
   > a {
     font-size: 30px;
@@ -14,4 +22,5 @@ export const UserDropDown = styled(NavDropdown)`
 
 export const NavDropDownLink = styled(Link)`
   padding: 10px;
+  color: #000;
 `;
